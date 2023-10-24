57b9d8953ac6f1258db52b9edfc02b2b1bebabe8
dont-text-me <ivanbar3330@gmail.com>
2023-10-24 01:34:29 +0100

    format

--- book-microservice/src/main/java/uk/ac/york/eng2/books/events/BooksStreams.java
+++ book-microservice/src/main/java/uk/ac/york/eng2/books/events/BooksStreams.java
@@ -6,7 +6,6 @@ import static uk.ac.york.eng2.books.events.Topics.TOPIC_BOOK_READ_BY_DAY;
 import io.micronaut.configuration.kafka.serde.CompositeSerdeRegistry;
 import io.micronaut.configuration.kafka.streams.ConfiguredStreamBuilder;
 import io.micronaut.context.annotation.Factory;
-import io.micronaut.serde.exceptions.SerdeException;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import java.time.Duration;
@@ -26,17 +25,21 @@ public class BooksStreams {
     props.put(StreamsConfig.APPLICATION_ID_CONFIG, "books-metrics");
     props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_V2);
 
-    KStream<WindowedIdentifier, Long> stream = builder.stream(
-                    TOPIC_BOOK_READ, Consumed.with(Serdes.Long(), serdeRegistry.getSerde(Book.class)))
+    KStream<WindowedIdentifier, Long> stream =
+        builder.stream(
+                TOPIC_BOOK_READ, Consumed.with(Serdes.Long(), serdeRegistry.getSerde(Book.class)))
             .selectKey((k, v) -> v.getId())
             .groupByKey(Grouped.with(Serdes.Long(), serdeRegistry.getSerde(Book.class)))
             .windowedBy(
-                    TimeWindows.ofSizeWithNoGrace(Duration.ofHours(24)).advanceBy(Duration.ofHours(24)))
+                TimeWindows.ofSizeWithNoGrace(Duration.ofHours(24)).advanceBy(Duration.ofHours(24)))
             .count()
             .toStream()
-            .selectKey((k, v) -> new WindowedIdentifier(k.key(), k.window().start(), k.window().end()));
+            .selectKey(
+                (k, v) -> new WindowedIdentifier(k.key(), k.window().start(), k.window().end()));
 
-    stream.to(TOPIC_BOOK_READ_BY_DAY, Produced.with(serdeRegistry.getSerde(WindowedIdentifier.class), Serdes.Long()));
+    stream.to(
+        TOPIC_BOOK_READ_BY_DAY,
+        Produced.with(serdeRegistry.getSerde(WindowedIdentifier.class), Serdes.Long()));
 
     return stream;
   }
